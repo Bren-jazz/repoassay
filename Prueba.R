16d1235b13e634656c95100cb8f48fb14fecb9dc
Bren-jazz <brenda.yvhdz@gmail.com>
2020-06-30 23:36:04 -0500

    Primer cambio de código

--- Prueba.R
+++ Prueba.R
@@ -1,1 +1,6 @@
-Esta es una prueba de repositorio de Github conectado a R
+##  Esta es una prueba de repositorio de Github conectado a R
+
+## Realizaremos algunas modificaciones en código
+
+print("Hello world")
+
